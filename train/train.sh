7611ddf5279199a44f2e8d41b69770909990a1b7
KuldeepKulkarni <kuldeep22.undefined@gmail.com>
2016-06-21 13:35:15 -0400

    Update train.sh

--- train/train.sh
+++ train/train.sh
@@ -1,3 +1,3 @@
-mkdir reconnet_0_01 #Create a folder to save the caffe models (measurement rate is 0.01)
+mkdir reconnet_0_04 #Create a folder to save the caffe models (measurement rate is 0.04)
 
 ../../../build/tools/caffe train --solver ReconNet_solver.prototxt
