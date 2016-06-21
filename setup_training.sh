48e5fc682fc5c7f3f98df1804e56f5b29c8f8caf
KuldeepKulkarni <kuldeep22.undefined@gmail.com>
2016-06-21 17:04:11 -0400

    Add files via upload

---
+++ setup_training.sh
@@ -0,0 +1,10 @@
+cd Train
+
+# Get training data (Note that this data is downloaded from SRCNN website: http://mmlab.ie.cuhk.edu.hk/projects/SRCNN.html)
+
+wget http://www.public.asu.edu/~kkulkar1/Training_Data.zip
+
+unzip Training_Data.zip
+
+rm Training_Data.zip
+
